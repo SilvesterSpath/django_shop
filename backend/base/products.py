f40744b9310dde7da2450f767b7d8e196740b916
SilvesterSpath <spath.sz@gmail.com>
2024-06-14 09:53:02 +0200

    products.py

---
+++ backend/base/products.py
@@ -0,0 +1,82 @@
+products = [
+  {
+    '_id': '1',
+    'name': 'Epiphone Les Paul Electric Guitar Blue',
+    'image': '/images/airpods.jpg',
+    'description':
+      'Tommy Thayer Electric Blue Les Paul OutfitFeaturing Seymour Duncan JB humbuckers and new EpiLite Case KISS legend Tommy “Spaceman” Thayer returns with his fourth Epiphone signature guitar, the Tommy Thayer Electric Blue Les Paul Outfit.',
+    'brand': 'Les Paul',
+    'category': 'Electronics',
+    'price': 849.99,
+    'countInStock': 10,
+    'rating': 4.5,
+    'numReviews': 12,
+  },
+  {
+    '_id': '2',
+    'name': 'Lilientahl Berlin Watch Radiant Blue',
+    'image': '/images/phone.jpg',
+    'description':
+      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+    'brand': 'Apple',
+    'category': 'Electronics',
+    'price': 299.99,
+    'countInStock': 7,
+    'rating': 4.0,
+    'numReviews': 8,
+  },
+  {
+    '_id': '3',
+    'name': 'Nike Air Max Blue Ocean Hyper',
+    'image': '/images/camera.jpg',
+    'description':
+      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+    'brand': 'Cannon',
+    'category': 'Electronics',
+    'price': 80.99,
+    'countInStock': 0,
+    'rating': 3,
+    'numReviews': 12,
+  },
+  {
+    '_id': '4',
+    'name': 'Frederick Constant Horological SmartWatch',
+    'image': '/images/playstation.jpg',
+    'description':
+      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+    'brand': 'Sony',
+    'category': 'Electronics',
+    'price': 599.99,
+    'countInStock': 11,
+    'rating': 5,
+    'numReviews': 12,
+  },
+  {
+    '_id': '5',
+    'name': 'Bluetooth Wireless Headset Sport Stereo',
+    'image': '/images/mouse.jpg',
+    'description':
+      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+    'brand': 'Logitech',
+    'category': 'Electronics',
+    'price': 28.99,
+    'countInStock': 7,
+    'rating': 3.5,
+    'numReviews': 10,
+  },
+  {
+    '_id': '6',
+    'name': 'Apple SmartWatch Series 5 SE Cellular',
+    'image': '/images/alexa.jpg',
+    'description':
+      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+    'brand': 'Amazon',
+    'category': 'Electronics',
+    'price': 289.99,
+    'countInStock': 0,
+    'rating': 4,
+    'numReviews': 12,
+  },
+];
+
+

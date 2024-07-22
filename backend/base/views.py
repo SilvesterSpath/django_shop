e963b965601ee291c551ff229ba1c5331b21b3d4
SilvesterSpath <spath.sz@gmail.com>
2024-07-22 06:40:38 +0200

    getting username and email with token

--- backend/base/views.py
+++ backend/base/views.py
@@ -13,20 +13,22 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    """ def validate(self, attrs):
+    def validate(self, attrs):
         data = super().validate(attrs)
         refresh = self.get_token(self.user)
-        data['refresh'] = str(refresh)
-        data['access'] = str(refresh.access_token)
-        return data """
-    @classmethod
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+        return data
+    
+    # config jwt token
+    """ @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
 
         # Add custom claims
         token['username'] = user.username
         token['message'] = 'hello world'
-        return token
+        return token """
     
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer

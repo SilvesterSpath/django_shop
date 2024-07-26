50212cffa597d8132c72d08822a29a36cf58fcef
SilvesterSpath <spath.sz@gmail.com>
2024-07-26 06:16:16 +0200

    error handling POST user

--- backend/base/views.py
+++ backend/base/views.py
@@ -13,8 +13,8 @@ from .serializers import ProductSerializer, UserSerializer, UserSerializerWithTo
 # Create your views here.
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-
 from django.contrib.auth.hashers import make_password
+from rest_framework import status
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
@@ -59,15 +59,18 @@ def getRoutes(request):
 @api_view(['POST'])
 def registerUser(request):
     data = request.data
-
-    user = User.objects.create(
-        first_name=data['name'],
-        username=data['email'],
-        email=data['email'],
-        password=make_password(data['password'])
-    )
-    serializer = UserSerializerWithToken(user, many=False)
-    return Response(serializer.data)
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail': 'User with this email already exists.'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])

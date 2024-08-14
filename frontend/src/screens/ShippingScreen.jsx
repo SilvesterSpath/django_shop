e68a8cea31639c1d0fd457993540d8c0ddf94e1c
SilvesterSpath <spath.sz@gmail.com>
2024-08-14 06:22:44 +0200

    Shipping screen form fields

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -1,7 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import FormContainer from '../components/FormContainer';
+import { Form } from 'react-bootstrap';
 
 const ShippingScreen = () => {
-  return <div>ShippingScreen</div>;
+  const [address, setAddress] = useState('');
+  const [city, setCity] = useState('');
+  const [postalCode, setPostalCode] = useState('');
+  const [country, setCountry] = useState('');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log('Submitting...');
+  };
+  return (
+    <FormContainer>
+      <h1>Shipping</h1>
+      <Form onSubmit={submitHandler}></Form>
+      <Form.Group controlId='address'>
+        <Form.Label>Address</Form.Label>
+        <Form.Control
+          required
+          type='text'
+          placeholder='Enter address'
+          value={address ? address : ''}
+          onChange={(e) => setAddress(e.target.value)}
+        ></Form.Control>
+      </Form.Group>
+      <Form.Group controlId='city'>
+        <Form.Label>City</Form.Label>
+        <Form.Control
+          required
+          type='text'
+          placeholder='Enter city'
+          value={city ? city : ''}
+          onChange={(e) => setCity(e.target.value)}
+        ></Form.Control>
+      </Form.Group>
+      <Form.Group controlId='postalCode'>
+        <Form.Label>Postal Code</Form.Label>
+        <Form.Control
+          required
+          type='text'
+          placeholder='Enter postal code'
+          value={postalCode ? postalCode : ''}
+          onChange={(e) => setPostalCode(e.target.value)}
+        ></Form.Control>
+      </Form.Group>
+      <Form.Group controlId='country'>
+        <Form.Label>Country</Form.Label>
+        <Form.Control
+          required
+          type='text'
+          placeholder='Enter country'
+          value={country}
+          onChange={(e) => setCountry(e.target.value)}
+        ></Form.Control>
+      </Form.Group>
+    </FormContainer>
+  );
 };
 
 export default ShippingScreen;

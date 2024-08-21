15cc72fd19a1e9e6154031fd138b9373a71da19d
SilvesterSpath <spath.sz@gmail.com>
2024-08-21 06:11:25 +0200

    payment screen

--- frontend/src/screens/PaymentScreen.jsx
+++ frontend/src/screens/PaymentScreen.jsx
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import FormContainer from '../components/FormContainer';
+import CheckoutSteps from '../components/CheckoutSteps';
+import { Button, Col, Form } from 'react-bootstrap';
 
 const PaymentScreen = () => {
-  return <div>PaymentScreen</div>;
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [paymentMethod, setPaymentMethod] = useState('PayPal');
+
+  /*   if (!shippingAddress.address) {
+    navigate('/shipping');
+  } */
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    /* dispatch(savePaymentMethod(paymentMethod)); */
+    navigate('/placeorder');
+  };
+
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 step3 />
+      <Form onSubmit={submitHandler}>
+        <Form.Group>
+          <Form.Label as='legend'>Select Method</Form.Label>
+          <Col>
+            <Form.Check
+              type='radio'
+              label='PayPal or Credit Card'
+              id='paypal'
+              name='paymentMethod'
+              value='PayPal'
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check>
+            <Form.Check
+              type='radio'
+              label='Stripe'
+              id='paypal'
+              name='paymentMethod'
+              value='Stripe'
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check>
+          </Col>
+        </Form.Group>
+        <Button type='submit' variant='primary'>
+          Continue
+        </Button>
+      </Form>
+    </FormContainer>
+  );
 };
 
 export default PaymentScreen;

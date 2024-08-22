2c680b375d898373b015298753aaedc242501c55
SilvesterSpath <spath.sz@gmail.com>
2024-08-22 05:57:17 +0200

    checkoutsteps as Link

--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
-import FormContainer from '../components/FormContainer';
 import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { saveShippingAddress } from '../actions/cartActions';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import CheckoutSteps from '../components/CheckoutSteps';
+import Message from '../components/Message';
 
 const PlaceOrderScreen = () => {
-  return <div>PlaceOrderScreen</div>;
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { shippingAddress } = cart;
+  return (
+    <div>
+      <CheckoutSteps step1 step2 step3 step4 />
+    </div>
+  );
 };
 
 export default PlaceOrderScreen;

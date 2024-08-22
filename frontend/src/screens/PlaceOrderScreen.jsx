5bbdbf070ddd1a725dc6cf1417ab04b505d19ff5
SilvesterSpath <spath.sz@gmail.com>
2024-08-22 10:31:16 +0200

    paymentMehtod from localStorage

--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -8,13 +8,34 @@ import Message from '../components/Message';
 
 const PlaceOrderScreen = () => {
   const cart = useSelector((state) => state.cart);
+  console.log(cart);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const { shippingAddress } = cart;
   return (
     <div>
       <CheckoutSteps step1 step2 step3 step4 />
+      <Row>
+        <Col md={8}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>Shipping</h2>
+              <p>
+                <strong>Shipping: </strong> {cart.shippingAddress.address}{' '}
+                {cart.shippingAddress.city} {cart.shippingAddress.postalCode}{' '}
+                {cart.shippingAddress.country}
+              </p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h2>Payment Method</h2>
+              <p>
+                <strong>Payment: </strong> {cart.paymentMethod}
+              </p>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}></Col>
+      </Row>
     </div>
   );
 };
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -31,14 +31,19 @@ const userInfoFromLocalStorage = localStorage.getItem('userInfo')
   ? JSON.parse(localStorage.getItem('userInfo'))
   : null;
 
-const shippingAddressFromStorage = localStorage.getItem('shiippingAddress')
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
   ? JSON.parse(localStorage.getItem('shippingAddress'))
+  : {};
+
+const paymentMethodFromStorage = localStorage.getItem('paymentMethod')
+  ? JSON.parse(localStorage.getItem('paymentMethod'))
   : {};
 
 const initialState = {
   cart: {
     cartItems: cartItemsFromLocalStorage,
     shippingAddress: shippingAddressFromStorage,
+    paymentMethod: paymentMethodFromStorage,
   },
   userLogin: { userInfo: userInfoFromLocalStorage },
 };

df90ab3b597dff12ab0ffb33ea962887aff52b61
SilvesterSpath <spath.sz@gmail.com>
2024-09-30 07:22:38 +0200

    edituserscreen

---
+++ frontend/src/screens/EditUserScreen.jsx
@@ -0,0 +1,107 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserDetails } from '../actions/userActions';
+import FormContainer from '../components/FormContainer';
+
+const EditUserScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [admin, setAdmin] = useState(false);
+
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const userRegister = useSelector((state) => state.userRegister);
+  const { loading, error, userInfo } = userRegister;
+
+  const redirect = location.search ? location.search.split('=')[1] : '/';
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, userInfo, redirect]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setMessage('Passwords do not match');
+    } else {
+      dispatch(register(name, email, password));
+    }
+  };
+
+  return (
+    <div>
+      <FormContainer>
+        <h1>Sign Up</h1>
+        {message && <Message variant='danger'>{message}</Message>}
+        {error && <Message variant='danger'>{error}</Message>}
+        {loading && <Loader />}
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId='name'>
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              required
+              type='text'
+              placeholder='Enter name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <p></p>
+          <Form.Group controlId='email'>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              required
+              type='email'
+              placeholder='Enter email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <p></p>
+          <Form.Group controlId='password'>
+            <Form.Label>Enter Password</Form.Label>
+            <Form.Control
+              required
+              type='password'
+              placeholder='Enter password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <p></p>
+          <Form.Group controlId='passwordConfirm'>
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              required
+              type='password'
+              placeholder='Confirm password'
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <p></p>
+          <Button type='submit' variant='primary'>
+            Sign Up
+          </Button>
+        </Form>
+      </FormContainer>
+      <Row className='py-3'>
+        <Col>
+          Have an account?{' '}
+          <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>
+            Sign in
+          </Link>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default EditUserScreen;

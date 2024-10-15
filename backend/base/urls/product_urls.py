32cbfc5f28a0d80c9db2ce2b9d7522f9358e3eed
SilvesterSpath <spath.sz@gmail.com>
2024-10-15 06:20:40 +0200

    product update url

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
   path('', views.getProducts, name='products'),
   path('create/', views.createProduct, name='product-create'),
   path('<str:pk>/', views.getProduct, name='product'),
+  path('update/<str:pk>/', views.updateProduct, name='product-update'),
   path('delete/<str:pk>/', views.deleteProduct, name='product-delete'),
-  path('update/<str:pk>/', views.deleteProduct, name='product-update'),
+
 ]
